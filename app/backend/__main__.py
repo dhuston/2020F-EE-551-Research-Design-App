5b80be0f2244495c271ec02c06fc4080fc3a3a23
dhuston <drhuston1@gmail.com>
2020-12-18 02:29:24 -0500

    schemas and comments

--- app/backend/__main__.py
+++ app/backend/__main__.py
@@ -1,1 +1,6 @@
 from __future__ import absolute_import, print_function
+
+# __future__ serves 3 purposes:
+# Avoid confusing existing tools that analyze import statements and to find the modules they’re importing.
+# Ensure future statements run under releases prior to 2.1 at least yield runtime exceptions.
+# Document when incompatible changes were introduced, and when they will be — or were — made mandatory.
